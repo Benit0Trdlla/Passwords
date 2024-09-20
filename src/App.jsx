d3231c87b8a42f6e9bf66ee22165ddc636c5e369
Benit0Trdlla <benilorenzo007op@gmail.com>
2024-09-20 11:11:06 -0300

    [New]: Modify Title, Added Favicon. Deploy.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="d-flex align-items-center justify-content-center vh-100">
         <div className="container d-flex flex-column pt-2 pt-lg-3 mt-lg-0 vh-100">
           <div className='p-3'>
-            <h1 className='text-center fs-2 fw-bold'>Gestor de Contraseñas Inteligente</h1>
+            <h1 className='text-center fs-2 fw-bold'>🔐 Genera y Gestiona Tus Contraseñas 🔑</h1>
             <div className='fs-6 mt-4 fw-light'>
               <p className='text-center'>
                 Genera y gestiona tus contraseñas de forma segura. Puedes crearlas automáticamente o personalizarlas con una palabra específica, con longitudes entre 8 y 16 caracteres. Guarda, copia, utiliza y elimina contraseñas fácilmente con una interfaz intuitiva.
